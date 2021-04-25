94f0f4033ed4c9af2f537f9f3119bdc70d6076cb
sebasgarcia29 <s.garcia@iimoney.co>
2021-04-24 20:18:31 -0500

    first commit

---
+++ src/navigation/StackNavigator.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{
+      headerShown: false,
+    }}>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
